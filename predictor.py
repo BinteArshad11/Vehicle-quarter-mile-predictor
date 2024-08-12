46082509d8907b902fbc5a0774992d224f2b2aea
Saliqa Arshad <saliqaarshad99@gmail.com>
2024-08-12 13:01:41 +0500

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -3,19 +3,9 @@ import streamlit as slt
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from math import sqrt
+import joblib
 
-# Import data
-df = pd.read_csv('mtcars.csv')
-
-# Split data into train and test sets
-X = df.drop(columns=['model', 'qsec'])
-y = df['qsec']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-# Train decision tree
-dt = DecisionTreeRegressor()
-dt.fit(X_train, y_train)
-
+dt=joblib.load("model_train.pkl")
 # Streamlit app
 slt.title('Quarter Mile Time Predictor')
 slt.write("Enter the following values for your vehicle:")
